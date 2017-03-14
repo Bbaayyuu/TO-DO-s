c66e4267ec95708e51bf3361e6cb6afe7357a73b
Bbaayyuu <marcgascon77@gmail.com>
2017-03-14 12:39:24 +0100

    SDL2
    continue

---
+++ SDL2_handout/ModuleWindow.cpp
@@ -0,0 +1,26 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleWindow.h"
+#include "SDL\include\SDL.h"
+
+// TODO 2: Init the library and check for possible error
+// using SDL_GetError()
+
+// TODO 3: pick the width and height and experiment with flags: borderless / fullscreen / resizable,
+// then create the window and check for errors
+
+// TODO 4: Finally create a screen surface and keep it as a public variable
+
+// TODO 5: Fill with code CleanUp() method :)
+
+
+bool ModuleWindow::Init()
+{
+	bool ret = true;
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		LOG(" Unable to initialize SDL; %s\n", SDL_GetError());
+		ret = false;
+	}
+	return ret;
+}
---
+++ SDL2_handout/ModuleWindow.h
@@ -0,0 +1,14 @@
+#ifndef __ModuleWindow_H__
+#define __ModuleWindow_H__
+#include "SDL/include/SDL.h"
+
+// TODO 1: Create the declaration of ModuleWindow class
+class ModuleWindow: public Module
+{
+public:
+
+	bool Init() { return true; }
+	bool CleanUp() { return true; }
+};
+
+#endif // __ModuleWindow_H__
